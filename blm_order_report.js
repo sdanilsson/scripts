ea1edcf6a068887071f9348e95a43f2be9926a96
Andreas Nilsson <andreas.nilsson@bydeluxe.com>
2016-01-25 16:34:25 -0800

    Finalized POC

--- blm_order_report.js
+++ blm_order_report.js
@@ -2,6 +2,7 @@
 var connection = new Mongo();
 var ravnDB = connection.getDB('ravn');
 var mediaIDs = [];
+var orders = new Object();
 
 function generateReport(){	
 	var op = {};
@@ -21,9 +22,46 @@ function generateReport(){
 
 
 	mediaIDs.forEach(function(id){
-		print(id);
+		// grab the info for the media ids
+		//db['ravn.media'].find({'v.id':'0000994d-1544-4bf7-8e85-d58cc01fe860'},{'v.insight-metadata.barcode':1,'v.insight-metadata.title':1,'v.mediaType':1})
+		var mediaObj = {};
+		mediaObj['v.id'] = id;
+		//var infoCursor = ravnDB['ravn.media'].find(mediaObj,{'v.insight-metadata.barcode':1,'v.insight-metadata.title':1,'v.mediaType':1})
+		var infoCursor = ravnDB['ravn.media'].find(mediaObj)
+		infoCursor.forEach( function(document) {
+			var barcode = document.v['insight-metadata'].barcode;
+			var type = document.v['mediaType'];
+			
+			orderObj = {};
+			orderObj['barcode'] = barcode;
+			orderObj['type'] = type;
+			orderObj['frequency'] = 1;
+
+			if(barcode in orders){
+				orders[barcode].frequency += 1;
+			} else {
+				orders[barcode] = orderObj;
+			}
+			
+		});
+		
 	});
+
+
+	// sort descending
+	var sorted = sortDescending(orders);
+
+	print("Barcode\tFrequency\tType");
 	
+	sorted.forEach(function(ord){
+		print(ord.barcode+"\t"+ord.frequency+"\t"+ord.type);
+	})
+
+	// for(var key in orders){
+ 	//  		print(orders[key].barcode+"\t"+orders[key].frequency+"\t"+orders[key].type);
+	// }
+
+	//print(orders['BLM00012361'].frequency);
 }
 
 function collectMediaIds(collectionName,obj){
@@ -55,6 +93,33 @@ function processSegment(collectionName,obj){
 			mediaIDs.push(mediaId);
 		});
 	});
+}
+
+
+function sortAscending(object){
+	var sortable = [];
+	for(var key in orders){
+		sortable.push(object[key]);
+	}
+	
+	var byFrequency = sortable.sort(function(a,b) {
+    return a.frequency - b.frequency;
+	});
+	
+	return byFrequency;
+}
+
+function sortDescending(object){
+	var sortable = [];
+	for(var key in orders){
+		sortable.push(object[key]);
+	}
+	
+	var byFrequency = sortable.sort(function(a,b) {
+    return b.frequency - a.frequency;
+	});
+	
+	return byFrequency;
 }
 
 generateReport();
