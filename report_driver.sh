f7494af4a8c0b8114f799a6b4dc1cf72f594773f
Andreas Nilsson <andreas.nilsson@bydeluxe.com>
2016-04-22 11:17:09 -0700

    added connection apple script - opens terminal window and connects

--- report_driver.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/mongo ravn /home/anilsson/ta_report_generator.js > /home/anilsson/ta_ingest_report.txt
-#mail -a /home/anilsson/ta_ingest_report.txt -s "Report for TA Ingest" -r cron@insight.ta.mediarecall.com andreas.nilsson@bydeluxe.com <<< "TA Ingest Report is attached."
